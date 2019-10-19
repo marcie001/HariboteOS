dafd6dd7be779c97d280da08ec1558c3cab4e6de
marcie001 <marcie00001@gmail.com>
2019-10-19 15:48:41 +0900

    4日目 harib01e相当

--- day1/bootpack.c
+++ day1/bootpack.c
@@ -1,13 +1,17 @@
 /* 他のファイルで作った関数がありますと C compiler に教える */
 void io_hlt(void);
 
-void write_mem8(int addr, int data);
-
 void HariMain(void) {
     int i;
+    char *p;
 
-    for (i = 0xa0000; i <= 0xaffff; i++) {
-        write_mem8(i, 15);
+    p = (char *) 0xa0000;
+    for (i = 0; i <= 0xffff; i++) {
+        // メモリ p + i 番地に i & 0x0f を書き込む
+        i[p] = i & 0x0f;
+        // p[i] や i[p] は *(p + i) と完全に同じ意味
+
+        // 78ページ Column-3, 82ページ Column-4 に詳しい解説がある
     }
 
     while (1) {
