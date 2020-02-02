317008ce936141ad8ea32e6b5ee382dceeb68f13
marcie001 <marcie00001@gmail.com>
2020-02-02 11:29:50 +0900

    format

--- day1/bootpack.h
+++ day1/bootpack.h
@@ -81,14 +81,14 @@ void load_gdtr(int limit, int adr);
 
 void load_idtr(int limit, int adr);
 
-#define ADR_IDT            0x0026f800
-#define LIMIT_IDT        0x000007ff
-#define ADR_GDT            0x00270000
-#define LIMIT_GDT        0x0000ffff
-#define ADR_BOTPAK        0x00280000
-#define LIMIT_BOTPAK    0x0007ffff
-#define AR_DATA32_RW    0x4092
-#define AR_CODE32_ER    0x409a
+#define ADR_IDT 0x0026f800
+#define LIMIT_IDT 0x000007ff
+#define ADR_GDT 0x00270000
+#define LIMIT_GDT 0x0000ffff
+#define ADR_BOTPAK 0x00280000
+#define LIMIT_BOTPAK 0x0007ffff
+#define AR_DATA32_RW 0x4092
+#define AR_CODE32_ER 0x409a
 
 /* mysprintf.c */
 void mysprintf(char *str, char *fmt, ...);
