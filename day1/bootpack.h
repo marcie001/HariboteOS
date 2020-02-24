19d46e536a6f2cdd5adfe4188ead1b8b061e548e
marcie001 <marcie00001@gmail.com>
2020-02-24 11:47:28 +0900

    9日目 harib07a相当
    10進数での切り捨ては i = i - (i % 切り捨て単位)

--- day1/bootpack.h
+++ day1/bootpack.h
@@ -162,6 +162,35 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 extern struct FIFO8 mousefifo;
 
+/* memory.c */
+#define MEMMAN_ADDR 0x003c0000
+#define MEMMAN_FREES 4090 // 4090でメモリ管理領域に約32KB使う
+
+/**
+ * メモリ空き情報
+ */
+struct FREEINFO {
+    unsigned int addr, size;
+};
+
+/**
+ * メモリ管理
+ */
+struct MEMMAN {
+    int frees, maxfrees, lostsize, losts;
+    struct FREEINFO free[MEMMAN_FREES];
+};
+
+unsigned int memtest(unsigned int start, unsigned int end);
+
+void memman_init(struct MEMMAN *man);
+
+unsigned int memman_total(struct MEMMAN *man);
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
 /* mysprintf.c */
 void mysprintf(char *str, char *fmt, ...);
 
