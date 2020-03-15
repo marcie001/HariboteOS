6e315730bcda51253e8dcee2b5b5ed4a4af4dc2b
marcie001 <marcie00001@gmail.com>
2020-03-15 17:25:56 +0900

    12日目終了

--- day1/timer.c
+++ day1/timer.c
@@ -15,32 +15,45 @@ void init_pit(void) {
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
+    timerctl.next = 0xffffffff;
+    timerctl.using = 0;
     for (int i = 0; i < MAX_TIMER; ++i) {
-        timerctl.timer[i].flags = 0; // 未使用状態を表す
+        timerctl.timers0[i].flags = 0; // 未使用状態を表す
     }
     return;
 }
 
 void inthandler20(int *esp) {
+    int i, j;
     io_out8(PIC0_OCW2, 0x60); // IRQ-00受付完了をPICに通知
     timerctl.count++;
-    for (int i = 0; i < MAX_TIMER; ++i) {
-        if (timerctl.timer[i].flags == TIMER_FLAGS_USING) {
-            timerctl.timer[i].timeout--;
-            if (timerctl.timer[i].timeout == 0) {
-                timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
-                fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
-            }
+    if (timerctl.next > timerctl.count) {
+        return;
+    }
+    for (i = 0; i < timerctl.using; ++i) {
+        if (timerctl.timers[i]->timeout > timerctl.count) {
+            break;
         }
+        timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
+        fifo8_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+    }
+    timerctl.using -= i;
+    for (j = 0; j < timerctl.using; ++j) {
+        timerctl.timers[j] = timerctl.timers[i + j];
+    }
+    if (timerctl.using > 0) {
+        timerctl.next = timerctl.timers[0]->timeout;
+    } else {
+        timerctl.next = 0xffffffff;
     }
     return;
 }
 
 struct TIMER *timer_alloc(void) {
     for (int i = 0; i < MAX_TIMER; ++i) {
-        if (timerctl.timer[i].flags == 0) {
-            timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
-            return &timerctl.timer[i];
+        if (timerctl.timers0[i].flags == 0) {
+            timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+            return &timerctl.timers0[i];
         }
     }
     return 0; // 空いているタイマがみつからなかった
@@ -58,7 +71,24 @@ void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data) {
 }
 
 void timer_settime(struct TIMER *timer, unsigned int timeout) {
-    timer->timeout = timeout;
+    timer->timeout = timerctl.count + timeout;
     timer->flags = TIMER_FLAGS_USING;
+    int e = io_load_eflags();
+    io_cli();
+    // どこにいれればいいか探す
+    int i;
+    for (i = 0; i < timerctl.using; ++i) {
+        if (timerctl.timers[i]->timeout >= timer->timeout) {
+            break;
+        }
+    }
+    for (int j = timerctl.using; j > i; j--) {
+        timerctl.timers[j] = timerctl.timers[j - 1];
+    }
+    timerctl.using++;
+    // あいたすきまにいれる
+    timerctl.timers[i] = timer;
+    timerctl.next = timerctl.timers[0]->timeout;
+    io_store_eflags(e);
     return;
 }
