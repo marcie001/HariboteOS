8f1ec9af6921fbcb9ad5103a7ff7f673bb904d0b
marcie001 <marcie00001@gmail.com>
2020-03-29 16:27:24 +0900

    キー入力の実装 harib11g相当
    カウンタは削除した

--- day1/bootpack.c
+++ day1/bootpack.c
@@ -10,13 +10,26 @@ void HariMain(void) {
     char s[40];
     int fifobuf[128];
     struct TIMER *timer, *timer2, *timer3;
-    int mx, my, i, count = 0;
+    int mx, my, i;
     unsigned int memtotal;
     struct MOUSE_DEC mdec;
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
     struct SHTCTL *shtctl;
     struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_win_mem;
     unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_win_mem;
+    // http://oswiki.osask.jp/?(AT)keyboard
+    // @formatter:off
+    static char keytable[0x80] = {
+            0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+            'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+            'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+            0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+            '2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+            0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+            0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '\\',0,   0,
+    };
+    // @formatter:on
 
     // ハードウェアがいろいろなデータを溜め込んで不具合を起こさないうちに早く割り込みを受け付けられるように
     // まずGDT/IDTを作り直し、PICを初期化してio_stiを呼び出す
@@ -62,7 +75,7 @@ void HariMain(void) {
     sheet_setbuf(sht_win_mem, buf_win_mem, 280, 52, -1);
     init_screen(buf_back, binfo->scrnx, binfo->scrny);
     init_mouse_cursor8(buf_mouse, 99);
-    make_window8(buf_win, 160, 52, "counter");
+    make_window8(buf_win, 160, 52, "window");
     make_window8(buf_win_mem, 280, 52, "memory");
     sheet_slide(sht_back, 0, 0);
     sheet_slide(sht_win, 80, 72);
@@ -80,17 +93,23 @@ void HariMain(void) {
     putfonts8_asc_sht(sht_back, 0, 132, COL8_FFFFFF, COL8_008484, s, 15);
 
     while (1) {
-        count++;
         io_cli();
         if (fifo32_status(&fifo) == 0) {
-            io_sti();
+            io_stihlt();
         } else {
             i = fifo32_get(&fifo);
             io_sti();
             if (256 <= i && i <= 511) {
                 // キーボードデータ
-                mysprintf(s, "%x", i);
+                mysprintf(s, "%x", i - 256);
                 putfonts8_asc_sht(sht_back, 0, 116, COL8_FFFFFF, COL8_008484, s, 4);
+                if (i < 256 + 0x80) {
+                    if (keytable[i - 256] != 0) {
+                        s[0] = keytable[i - 256];
+                        s[1] = 0;
+                        putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 1);
+                    }
+                }
             } else if (512 <= i && i <= 767) {
                 // マウスデータ
                 if (mouse_decode(&mdec, i) != 0) {
@@ -125,11 +144,8 @@ void HariMain(void) {
                 }
             } else if (i == 10) {
                 putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
-                mysprintf(s, "%d", count);
-                putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
             } else if (i == 3) {
                 putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
-                count = 0; // 計測開始
             } else if (i == 1) {
                 timer_init(timer3, &fifo, 0); // 次はデータを0とする
                 boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
