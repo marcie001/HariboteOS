e71a2697eda4aad0231103970ea9d5e52674c2a4
marcie001 <marcie00001@gmail.com>
2020-05-04 15:08:37 +0900

    21日目 C でアプリを書けるようになった harib18b 相当
    ただし、コンパイルしてできたa.hrb, hello3.hrbはそのままでは動かない。
    バイナリエディタで開き、先頭6バイトを下のように書き換えると動く
    
    ```
    E8 16 00 00 00 CB
    ```

---
+++ day1/a.c
@@ -0,0 +1,10 @@
+//
+// Created by marcie on 2020/05/04.
+//
+
+void api_putchar(int c);
+
+void HariMain(void) {
+    api_putchar('A');
+    return;
+}
---
+++ day1/hello3.c
@@ -0,0 +1,14 @@
+//
+// Created by marcie on 2020/05/04.
+//
+
+void api_putchar(int c);
+
+void HariMain(void) {
+    api_putchar('H');
+    api_putchar('e');
+    api_putchar('l');
+    api_putchar('l');
+    api_putchar('o');
+    return;
+}
