c250f0c3fee31040b748924e2e6bbc65ce597cc1
marcie001 <marcie00001@gmail.com>
2020-07-19 09:08:27 +0900

    26日目 ❌ボタンでコンソールを閉じれるようにした harib23h 相当

--- day1/console.c
+++ day1/console.c
@@ -71,6 +71,9 @@ void console_task(struct SHEET *sheet, unsigned int memtotal) {
                 boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cons.cur_x, 28, cons.cur_x + 7, 43);
                 cons.cur_c = -1;
             }
+            if (i == 4) {
+                cmd_exit(&cons, fat);
+            }
             if (256 <= i && i <= 511) {
                 // キーボードデータ
                 if (i == 8 + 256) {
