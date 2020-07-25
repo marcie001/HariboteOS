ec152439d5ea21953881e05424a4cbd795f02a6e
marcie001 <marcie00001@gmail.com>
2020-07-25 16:07:28 +0900

    29日目 非矩形ウィンドウアプリの追加 harib26c 相当
    また、アプリ作成スクリプトを追加

---
+++ day1/notrec/notrec.c
@@ -0,0 +1,19 @@
+//
+// Created by marcie on 2020/07/25.
+//
+#include "libapi.h"
+
+void HariMain(void) {
+    int win;
+    char buf[150 * 70];
+    win = api_openwin(buf, 150, 70, 255, "notrec");
+    api_boxfilwin(win, 0, 50, 34, 69, 255);
+    api_boxfilwin(win, 115, 50, 149, 69, 255);
+    api_boxfilwin(win, 50, 30, 99, 49, 255);
+    while (1) {
+        if (api_getkey(1) == 0x0a) {
+            break;
+        }
+    }
+    api_end();
+}
